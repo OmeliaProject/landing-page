9b6931fc9d69c410b53595f5e3f343f8058bbb01
robin.cadinot <robin.cadinot@epitech.eu>
2021-05-09 17:58:21 +0200

    feat: 'textarea' de contact dynamique

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-const contactSend = document.querySelector('#contact');
+const contactSend = document.querySelector('#contact-send');
 const contactValue = document.querySelector('#contact-value');
 
 
 contactValue.addEventListener('change', (event) => {
-    contactSend.action = "mailto:omelia.contact@gmail.com?subject=Visiteur%20Site%20Vitrine&body=" + contactValue.value
-    console.log(contactSend.action)
+    contactSend.href = "mailto:omelia.contact@gmail.com?subject=Visiteur%20Site%20Vitrine&body=" + contactValue.value
 });
