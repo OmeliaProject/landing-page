40b7448350aca9578018f35ddd4683e2398bd05c
mathiasvgr <mathias.vigier@epitech.eu>
2022-09-14 18:01:14 +0200

    FEAT: Modal working on monitoring

--- components/pages/beta/monitoring/Index.tsx
+++ components/pages/beta/monitoring/Index.tsx
@@ -3,26 +3,47 @@ import Head from "next/head";
 
 import styles from "@styles/pages/monitoring.module.css";
 import { NavbarBeta } from "@components/commons/NavbarBeta";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { IIssue } from "@components/api/types/IIssue";
 import useTransportLayer from "@hooks/useTransportLayer";
+import { ModalContext } from "@components/api/modalContext";
 
 
 interface MonitoringProps {
     
 }
+
+
+const timestampToDate = (timestamp: number) => {
+    const date = new Date(timestamp);
+    return date.toLocaleDateString();
+};
+
+const ModalIssue = (issue : IIssue) => {
+    return (
+        <>
+            <h1 className={styles.modal_title}>{issue.title}</h1>
+            <p className={styles.modal_body}>{issue.body}</p>
+            <div className={styles.modal_info_container}>
+                <p>date: {timestampToDate(issue.timestamp)}</p>
+                <div>{"nombre de j'aime: " + issue.likes}</div>
+            </div>
+        </>
+    );
+}
+
  
 const Monitoring : NextPage<MonitoringProps> = () => {
 
     // all issues from the api
     const [issues, setIssues] = useState<IIssue[]>([]);
+    const { handleModal } = useContext(ModalContext);
     const api = useTransportLayer();
 
     // useEFfect to get all issues
     useEffect(() => {
         api.issues.getIssues().then((issues) => {
             setIssues(issues);
-            console.log(issues);
         });
     }, []);
 
@@ -31,14 +52,6 @@ const Monitoring : NextPage<MonitoringProps> = () => {
             setIssues(issues.filter((issue) => issue.id !== id));
         });
     };
-
-
-    const timestampToDate = (timestamp: number) => {
-        const date = new Date(timestamp);
-        return date.toLocaleDateString();
-    };
-
-    // table will have : title, body, date, hasliked
 
     return (
         <>
@@ -61,19 +74,21 @@ const Monitoring : NextPage<MonitoringProps> = () => {
                             </tr>
                         </thead>
                         <tbody className={styles.tbody}>
-                            {issues && issues.map((issue, idx) => (
-                                <tr key={idx} className={styles.tr}>
-                                    <td className={styles.td}>{idx + 1}</td>
-                                    <td className={styles.td}>{issue.title}</td>
-                                    <td className={styles.td}>{issue.body}</td>
-                                    <td className={styles.td}>{timestampToDate(issue.timestamp)}</td>
-                                    <td className={styles.td}>{issue.likes}</td>
-                                    <td className={styles.td}>
-                                        <div className={styles.delete} onClick={() => deleteIssue(issue.id)}>Supprimer</div>
-                                    </td>
-
-                                </tr>
-                            ))}
+                            {issues && issues.map((issue, idx) => {
+                                    return (
+                                        <tr onClick={() => handleModal(ModalIssue(issue))} key={idx} className={styles.tr}>
+                                            <td className={styles.td}>{idx + 1}</td>
+                                            <td className={styles.td}>{issue.title}</td>
+                                            <td className={styles.td}>{issue.body}</td>
+                                            <td className={styles.td}>{timestampToDate(issue.timestamp)}</td>
+                                            <td className={styles.td}>{issue.likes}</td>
+                                            <td className={styles.td}>
+                                                <div className={styles.delete} onClick={() => deleteIssue(issue.id)}>Supprimer</div>
+                                            </td>
+                                        </tr>
+                                    );
+                                })
+                            }
                         </tbody>
                     </table>
                 </div>
